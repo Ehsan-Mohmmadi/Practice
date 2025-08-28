72cda1bfb0bcd84c24e12018a9f2c8dbadce2f62
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-28 14:12:28 +0330

    Update views.py
    Add home view

--- home_module/views.py
+++ home_module/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, TemplateView
+
 
 # Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'shared/_layout.html'

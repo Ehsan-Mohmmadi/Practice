b2014d1969175d1d90f2ac9b36efc6431cf61521
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-29 21:48:29 +0330

    slug and category field

--- product_module/models.py
+++ product_module/models.py
@@ -1,10 +1,26 @@
+from tkinter.constants import CASCADE
+
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100,verbose_name='نام دسته بندی')
+
+    class Meta:
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = "دسته بندی ها"
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='نام محصول')
     price = models.FloatField(verbose_name='قیمت')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='دسته بندی')
+    slug = models.SlugField()
     in_stock = models.BooleanField(verbose_name='موجود')
-    
+
     class Meta:
         verbose_name = "محصول"
         verbose_name_plural = "محصولات"
--- product_module/views.py
+++ product_module/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from product_module.models import Product
+
+
 def product_list(request):
-    return render(request, 'product_module/product_list.html')
+    return render(request, 'product_module/product_list.html', context={'products': Product.objects.all()})

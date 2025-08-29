4c88e909bda5f10e8f08d3f6ba9c84a1c6e4fc1b
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-29 13:47:01 +0330

    footer partial view

--- home_module/views.py
+++ home_module/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
 
 def header_component(request):
     return render(request,'shared/header_component.html')
+
+def content_component(request):
+    return render(request,'home_module/index.html')
+
+def footer_component(request):
+    return render(request, 'shared/footer_component.html')
+
 def home(request):
     return render(request,'shared/_layout.html')

44272813d347f8951170c8ea18f0c613f1f9f852
Ehsan MohammadAbadi <ehsan.mohmmadi2130@gmail.com>
2025-08-29 21:33:15 +0330

    Update models.py
    Add product model

--- product_module/models.py
+++ product_module/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='نام محصول')
+    price = models.FloatField(verbose_name='قیمت')
+    in_stock = models.BooleanField(verbose_name='موجود')
+    
+    class Meta:
+        verbose_name = "محصول"
+        verbose_name_plural = "محصولات"
+
+    def __str__(self):
+        return self.name
